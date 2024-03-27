838d1036b06065fb81e02e0f69b39c297e89004e
Sumit Bopate <sumitbopate888@gmail.com>
2024-03-27 23:28:23 +0530

    Add feature

--- Day2/index.js
+++ Day2/index.js
@@ -6,13 +6,28 @@ let alert=document.getElementById('alert');
 // let deleteItem=document.getElementById("delete-task");  
 function addTask(){
     let taskText=todo_input.value;
-    if(taskText==''){
-        alert.innerText="No task Added"
-    }
+    
    if(taskText.trim() !== ''){
     let newTask=document.createElement('div');
     newTask.textContent=taskText;
     newTask.className='todo-task';
+     // raduo button
+            // Create radio buttons for complete/incomplete
+            let completeRadio = document.createElement('input');
+            completeRadio.className="input"
+            completeRadio.type = 'radio';
+            completeRadio.name = 'taskStatus';
+            completeRadio.value = 'complete';
+            completeRadio.addEventListener('change', function() {
+              newTask.style.textDecoration = 'line-through'; 
+              newTask.style.color = '#808080'; 
+
+            });
+         
+    
+            completeRadio.style.order = -1;
+            newTask.appendChild(completeRadio);
+        // newTask.appendChild(incompleteRadio);
 
       // Create a delete button for each task
       let deleteButton = document.createElement('button');
@@ -23,25 +38,33 @@ function addTask(){
         // deleteButton.appendChild(icon)
         icon.addEventListener('click', deleteTask); 
         deleteButton.appendChild(icon)
-    newTask.appendChild(icon); 
+    newTask.appendChild(icon);
+    
+   
     taskContainer.appendChild(newTask);
     todo_input.value='';
-    console.log("Task added:", taskText);
    
-   } 
- 
- console.log("added")
+    console.log("Task added:", taskText);
+
+}else{
+  alert.innerText="no task added"
+}
 }
 
 
 
-  button.addEventListener("click",addTask);
+
+ button.addEventListener("click",addTask);
+
 
 function deleteTask(event){
-    // Prevent the default action of the button click
+    
     event.preventDefault();
 
-    // Remove the parent task element from the DOM
-    let taskToDelete = event.target.parentElement;
+     let taskToDelete = event.target.parentElement;
     taskContainer.removeChild(taskToDelete);
+}
+function changeStatus(){
+  
+    newTask.style.textDecoration = 'line-through'; // Mark task as complete
 }
